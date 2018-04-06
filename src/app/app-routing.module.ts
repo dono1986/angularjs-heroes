fb49079fc5f85b093c38dc4886e51712be3caf6b
Tobias <donoghue1986@yahoo.de>
2018-04-06 15:00:54 +0200

    Added dashboard and detail view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import {HeroesComponent} from "./heroes/heroes.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {HeroDetailComponent} from "./hero-detail/hero-detail.component";
 
 // Routes tell the router which view to display when a user clicks a link or pastes a URL into the browser address bar.
 
 const routes: Routes = [
-  { path:'heroes', component: HeroesComponent }
+  { path:'', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path:'heroes', component: HeroesComponent },
+  { path:'dashboard', component: DashboardComponent },
+  { path:'detail/:id', component: HeroDetailComponent }
 ];
 
 @NgModule({
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,12 +4,19 @@ import {HEROES} from "./heroes/mock-heroes";
 import {Observable} from "rxjs/Observable";
 import {of} from "rxjs/observable/of";
 import {MessageService} from "./message.service";
+import {forEach} from "@angular/router/src/utils/collection";
 
 @Injectable()
 export class HeroService {
 
   constructor(private messageService: MessageService) { }
 
+  getHeroById(id: number): Observable<Hero> {
+
+    this.messageService.add(`Fetching hero with id=${id}`); //Note the backticks ( ` ) that define a JavaScript template literal for embedding the id.
+
+    return of(HEROES.find(heroValue => heroValue.id === id));
+  }
 
   getHeroes(): Observable<Hero[]> {
 
