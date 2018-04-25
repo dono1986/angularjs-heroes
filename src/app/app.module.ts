39a9e4233b3f354065615fb5e5cde1abe83b2aa0
Tobias <donoghue1986@yahoo.de>
2018-04-25 15:56:52 +0200

    added in memory database support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+
+import {HttpClientModule} from "@angular/common/http";
+import {HttpClientInMemoryWebApiModule} from "angular-in-memory-web-api";
+import {InMemoryDataService} from "./in-memory-data.service";
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
@@ -11,7 +14,7 @@ import {HeroService} from "./hero.service";
 import { MessagesComponent } from './messages/messages.component';
 import {MessageService} from "./message.service";
 import {AppRoutingModule} from "./app-routing.module";
-import { DashboardComponent } from './dashboard/dashboard.component';
+import {DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
@@ -24,8 +27,11 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, {dataEncapsulation: false}
+    )
   ],
   providers: [HeroService, MessageService], //generates a HeroService instance ready to import into any class requiring it
   bootstrap: [AppComponent]
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,28 +1,75 @@
 import { Injectable } from '@angular/core';
 import {Hero} from "./heroes/hero";
-import {HEROES} from "./heroes/mock-heroes";
+//import {HEROES} from "./heroes/mock-heroes";
 import {Observable} from "rxjs/Observable";
 import {of} from "rxjs/observable/of";
 import {MessageService} from "./message.service";
-import {forEach} from "@angular/router/src/utils/collection";
+
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+
+import {catchError, map, tap} from 'rxjs/operators'
+
+
 
 @Injectable()
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes'; // URL to web api; If the resource name (after the api base path) matches one of the configured collections, process that
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) { }
 
   getHeroById(id: number): Observable<Hero> {
 
-    this.messageService.add(`Fetching hero with id=${id}`); //Note the backticks ( ` ) that define a JavaScript template literal for embedding the id.
+    const url = `${this.heroesUrl}/${id}`;
 
-    return of(HEROES.find(heroValue => heroValue.id === id));
+    return this.http.get<Hero>(url).
+        pipe(
+          tap(_=>this.log(`fetched ${id}`)),
+          catchError(this.handleErrors<Hero>(`getHeroById id=${id}`))
+
+    );
+
+    //this.messageService.add(`Fetching hero with id=${id}`); //Note the backticks ( ` ) that define a JavaScript template literal for embedding the id.
+
+    //return (this.http.get<Hero>(this.heroesUrl))
+    //return of(HEROES.find(heroValue => heroValue.id === id));
   }
 
   getHeroes(): Observable<Hero[]> {
 
-    this.messageService.add("HeroService: fetched heroes");
+    return this.http.get<Hero[]>(this.heroesUrl).
+            pipe(
 
-    return of(HEROES);
+              //The HeroService methods will tap into the flow of observable values and send a message (via log()) to the message area at the bottom of the page.
+              // The tap operator looks at the observable values, does something with those values, and passes them along. The tap call back doesn't touch the values themselves.
+              tap(heroes=>this.log(`fetched ${heroes.length} heroes`),
+                catchError(this.handleErrors('getHeroes', [])))
+    );
+    //return of(HEROES);
   }
+
+  // Instead of handling the error directly, it returns an error handler function to catchError that it has configured with
+  // both the name of the operation that failed and a safe return value.
+  handleErrors<T>(operation = 'operation', result?: T) {
+    return (error:any) : Observable<T> => {
+      console.error(error);
+
+      this.log(`${operation} failed: ${error.message}`);
+
+      return of(result as T);
+    };
+  }
+
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add('HeroService: ' + message);
+  }
+
+
+
 
 }
